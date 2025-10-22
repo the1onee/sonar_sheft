5c9e7b643f9fb25b6b66aaf5a27a9b46f7af4a89
lenovo <mrluhy@gmail.com>
2025-10-22 12:27:03 +0300

    Fix ALLOWED_HOSTS with default value

--- shift_manager/shift_manager/settings.py
+++ shift_manager/shift_manager/settings.py
@@ -30,11 +30,8 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-change-in-production')
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 # المضيفات المسموح بها
-
-ALLOWED_HOSTS_STR = os.getenv('ALLOWED_HOSTS', '*')
-ALLOWED_HOSTS = [h.strip() for h in ALLOWED_HOSTS_STR.split(',') if h.strip()]
-if not ALLOWED_HOSTS:
-    ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS_STR = os.getenv('ALLOWED_HOSTS', 'shift-sonar.onrender.com,localhost,127.0.0.1,*')
+ALLOWED_HOSTS = [h.strip() for h in ALLOWED_HOSTS_STR.split(',')]
 
 
 
